51495a21c482e313802edf51afcf8a08599e2994
Markov Emil <69921054+EmilMarkov@users.noreply.github.com>
2025-07-27 04:16:26 +0300

    fix: убрал header Accepted-Language

--- src/vek_parser/http_client.py
+++ src/vek_parser/http_client.py
@@ -22,7 +22,6 @@ class HttpClient:
         self.session = requests.Session()
         default_headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
-            'Accept-Language': 'en-US,en;q=0.9',
             'Accept-Encoding': 'gzip, deflate, br'
         }
         self.session.headers.update(headers or default_headers)
